ab59d27901907c46adab157c966abcdabe6376e3
JiyuHe <he_jiyu@163.com>
2022-07-07 19:08:31 +0800

    V1.0

--- src/mainWindow.py
+++ src/mainWindow.py
@@ -16,7 +16,7 @@ class Ui_MainWindow(object):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(922, 573)
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("isGroup.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        icon.addPixmap(QtGui.QPixmap("ico\isGroup.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         MainWindow.setWindowIcon(icon)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
